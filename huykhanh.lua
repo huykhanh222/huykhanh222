0ef4c38616d9a65e91fc48f508b16bea9b3818e3
huykhanh222 <161200479+huykhanh222@users.noreply.github.com>
2024-03-22 11:06:16 +0700

    Update huykhanh.lua

--- huykhanh.lua
+++ huykhanh.lua
@@ -1,40 +1,109 @@
-getgenv().SpamSkill = false
+Online Notepad
+ Trang chủ
+Đăng Nhập
+Tạo Tài Khoản
+
+auto bounty
+getgenv().SpamSkill = true
+
 getgenv().AutoUseRaceV3 = true
+
 getgenv().AutoUseRacev4 = true
+
 getgenv().SpamSkillOnRaceV4 = true
+
 getgenv().Invisible = true
-getgenv().InCombatNoReset = true
+
+getgenv().InCombatNoReset = false
+
 getgenv().Team = "Pirates" -- Marines
-getgenv().TweenSpeed = 350 -- 350 max or Get Tp Back
- getgenv().Setting = { -- Select Weapon, Self Explain
-        ["Melee"] = {["Enable"] = true,["Delay"] = 2,
+
+getgenv().TweenSpeed = 300 -- 350 max or Get Tp Back
+
+getgenv().Setting = { -- Select Weapon, Self Explain
+
+        ["Melee"] = {["Enable"] = true,["Delay"] = 0,
+
           ["Skills"] = {
-            ["Z"] = {["Enable"] = true,["HoldTime"] = 0.3,["TimeToNextSkill"] = 0,},
+
+            ["Z"] = {["Enable"] = true,["HoldTime"] = 0,["TimeToNextSkill"] = 0,},
+
             [ "X"] = {["Enable"] = true,["HoldTime"] = 0, ["TimeToNextSkill"] = 0,},
-            ["C"] = {["Enable"] = true,["HoldTime"] = 0.1, ["TimeToNextSkill"] = 0,},
+
+            ["C"] = {["Enable"] = true,["HoldTime"] = 0, ["TimeToNextSkill"] = 0,},
+
             },
+
         },
-        ["Blox Fruit"] = {["Enable"] = true, ["Delay"] = 1.25,
+
+        ["Blox Fruit"] = {["Enable"] = false, ["Delay"] = 4,
+
             ["Skills"] = {
-                ["Z"] = {["Enable"] = true, ["HoldTime"] = 1, ["TimeToNextSkill"] = 0,},
+
+                ["Z"] = {["Enable"] = true, ["HoldTime"] = 0, ["TimeToNextSkill"] = 0,},
+
                 ["X"] = { ["Enable"] = true, ["HoldTime"] = 0, ["TimeToNextSkill"] = 0,},
+
                 ["C"] = { ["Enable"] = true, ["HoldTime"] = 0,["TimeToNextSkill"] = 0, },
+
                 ["V"] = { ["Enable"] = true, ["HoldTime"] = 0,["TimeToNextSkill"] = 0,},
-                ["F"] = {["Enable"] = true,["HoldTime"] = 0, ["TimeToNextSkill"] = 0,},
+
+                ["F"] = {["Enable"] = false,["HoldTime"] = 0, ["TimeToNextSkill"] = 0,},
+
             },
+
         },
-        ["Sword"] = { ["Enable"] = false, ["Delay"] = 0.7,
+
+        ["Sword"] = { ["Enable"] = true, ["Delay"] = 1,
+
             ["Skills"] = {
-                ["Z"] = {["Enable"] = true,  ["HoldTime"] = 0.3,["TimeToNextSkill"] = 0,},
-                ["X"] = {["Enable"] = true, ["HoldTime"] = 0, ["TimeToNextSkill"] = 0,},
+
+                ["Z"] = {["Enable"] = true,  ["HoldTime"] = 0.15,["TimeToNextSkill"] = 0,},
+
+                ["X"] = {["Enable"] = true, ["HoldTime"] = 0.5, ["TimeToNextSkill"] = 0,},
+
             },
+
         },
-        ["Gun"] = {["Enable"] = false, ["Delay"] = 2,
+
+        ["Gun"] = {["Enable"] = true, ["Delay"] = 0,
+
             ["Skills"] = {
-                ["Z"] = {["Enable"] = true,["HoldTime"] = 0.5,["TimeToNextSkill"] = 0,},
-                ["X"] = {["Enable"] = true,["HoldTime"] = 0.5,["TimeToNextSkill"] = 0,
+
+                ["Z"] = {["Enable"] = true,["HoldTime"] = 0.8,["TimeToNextSkill"] = 0,},
+
+                ["X"] = {["Enable"] = true,["HoldTime"] = 0.8,["TimeToNextSkill"] = 0,
+
                 },
+
             },
+
         }
+
     }
- loadstring(game:HttpGet('https://raw.githubusercontent.com/vinhuchi/temp-repos/main/FreeAutoBounty.lua'))()
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/vinhuchi/temp-repos/main/FreeAutoBounty.lua'))()
+
+ 
+
+vinhuchi — 18/11/2023 19:18
+
+getgenv().Team = "Pirates"
+
+getgenv().FixCrash = true -- Turn it On For Hopping Server, Improve Performance But Silent Aim On Mob And Player
+
+getgenv().FixCrash2 = true -- Turn it On For Hopping Server, Improve Performance But Will Remove Speed Changer
+
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b2169cf53bc6104dabe8e19562e5cc2.lua"))()
+
+
+ Update: 2024-01-31 20:18:38
+Report Child Abuse And Copyright (DMCA)
+
+
+
+Report Child Abuse And Copyright (DMCA)
+
+© INote.Pro 2017
+
+Trang Chủ |Tính Năng | Điều Khoản Sử Dụng | Chính Sách Bảo Mật
