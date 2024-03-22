e1810d48839ed59b9681403c86d7a0a1f84abf91
huykhanh222 <161200479+huykhanh222@users.noreply.github.com>
2024-03-22 11:10:46 +0700

    Update huykhanh.lua

--- huykhanh.lua
+++ huykhanh.lua
@@ -1,90 +1,40 @@
-getgenv().SpamSkill = true
-
+getgenv().SpamSkill = false
 getgenv().AutoUseRaceV3 = true
-
 getgenv().AutoUseRacev4 = true
-
 getgenv().SpamSkillOnRaceV4 = true
-
 getgenv().Invisible = true
-
-getgenv().InCombatNoReset = false
-
+getgenv().InCombatNoReset = true
 getgenv().Team = "Pirates" -- Marines
-
-getgenv().TweenSpeed = 300 -- 350 max or Get Tp Back
-
-getgenv().Setting = { -- Select Weapon, Self Explain
-
-        ["Melee"] = {["Enable"] = true,["Delay"] = 0,
-
+getgenv().TweenSpeed = 350 -- 350 max or Get Tp Back
+ getgenv().Setting = { -- Select Weapon, Self Explain
+        ["Melee"] = {["Enable"] = true,["Delay"] = 2,
           ["Skills"] = {
-
-            ["Z"] = {["Enable"] = true,["HoldTime"] = 0,["TimeToNextSkill"] = 0,},
-
+            ["Z"] = {["Enable"] = true,["HoldTime"] = 0.3,["TimeToNextSkill"] = 0,},
             [ "X"] = {["Enable"] = true,["HoldTime"] = 0, ["TimeToNextSkill"] = 0,},
-
-            ["C"] = {["Enable"] = true,["HoldTime"] = 0, ["TimeToNextSkill"] = 0,},
-
+            ["C"] = {["Enable"] = true,["HoldTime"] = 0.1, ["TimeToNextSkill"] = 0,},
             },
-
         },
-
-        ["Blox Fruit"] = {["Enable"] = false, ["Delay"] = 4,
-
+        ["Blox Fruit"] = {["Enable"] = true, ["Delay"] = 1.25,
             ["Skills"] = {
-
-                ["Z"] = {["Enable"] = true, ["HoldTime"] = 0, ["TimeToNextSkill"] = 0,},
-
+                ["Z"] = {["Enable"] = true, ["HoldTime"] = 1, ["TimeToNextSkill"] = 0,},
                 ["X"] = { ["Enable"] = true, ["HoldTime"] = 0, ["TimeToNextSkill"] = 0,},
-
                 ["C"] = { ["Enable"] = true, ["HoldTime"] = 0,["TimeToNextSkill"] = 0, },
-
                 ["V"] = { ["Enable"] = true, ["HoldTime"] = 0,["TimeToNextSkill"] = 0,},
-
-                ["F"] = {["Enable"] = false,["HoldTime"] = 0, ["TimeToNextSkill"] = 0,},
-
+                ["F"] = {["Enable"] = true,["HoldTime"] = 0, ["TimeToNextSkill"] = 0,},
             },
-
         },
-
-        ["Sword"] = { ["Enable"] = true, ["Delay"] = 1,
-
+        ["Sword"] = { ["Enable"] = false, ["Delay"] = 0.7,
             ["Skills"] = {
-
-                ["Z"] = {["Enable"] = true,  ["HoldTime"] = 0.15,["TimeToNextSkill"] = 0,},
-
-                ["X"] = {["Enable"] = true, ["HoldTime"] = 0.5, ["TimeToNextSkill"] = 0,},
-
+                ["Z"] = {["Enable"] = true,  ["HoldTime"] = 0.3,["TimeToNextSkill"] = 0,},
+                ["X"] = {["Enable"] = true, ["HoldTime"] = 0, ["TimeToNextSkill"] = 0,},
             },
-
         },
-
-        ["Gun"] = {["Enable"] = true, ["Delay"] = 0,
-
+        ["Gun"] = {["Enable"] = false, ["Delay"] = 2,
             ["Skills"] = {
-
-                ["Z"] = {["Enable"] = true,["HoldTime"] = 0.8,["TimeToNextSkill"] = 0,},
-
-                ["X"] = {["Enable"] = true,["HoldTime"] = 0.8,["TimeToNextSkill"] = 0,
-
+                ["Z"] = {["Enable"] = true,["HoldTime"] = 0.5,["TimeToNextSkill"] = 0,},
+                ["X"] = {["Enable"] = true,["HoldTime"] = 0.5,["TimeToNextSkill"] = 0,
                 },
-
             },
-
         }
-
     }
-
-loadstring(game:HttpGet('https://raw.githubusercontent.com/vinhuchi/temp-repos/main/FreeAutoBounty.lua'))()
-
- 
-
-
-getgenv().Team = "Pirates"
-
-getgenv().FixCrash = true -- Turn it On For Hopping Server, Improve Performance But Silent Aim On Mob And Player
-
-getgenv().FixCrash2 = true -- Turn it On For Hopping Server, Improve Performance But Will Remove Speed Changer
-
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b2169cf53bc6104dabe8e19562e5cc2.lua"))()
+ loadstring(game:HttpGet('https://raw.githubusercontent.com/vinhuchi/temp-repos/main/FreeAutoBounty.lua'))()
